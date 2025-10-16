929feeed56722ebfd6a7056b2b6855fb8d48c8f0
kelvfimer <calvarro@gmail.com>
2025-10-16 14:46:12 +0200

    ini changes and start script

--- packages/sx05re/emulators/PPSSPPSDL/scripts/ppsspp.sh
+++ packages/sx05re/emulators/PPSSPPSDL/scripts/ppsspp.sh
@@ -5,14 +5,19 @@
 
 . /etc/profile
 
+[ ! -f /storage/roms/savestates/PPSSPPSDL/PSP/SYSTEM/ppsspp.ini ] && mkdir -p /storage/roms/savestates/PPSSPPSDL/PSP/SYSTEM && cp -Rf /usr/config/ppsspp/PSP/SYSTEM/ppsspp.ini /storage/roms/savestates/PPSSPPSDL/PSP/SYSTEM/ppsspp.ini
+
+[ ! -f /storage/roms/savestates/PPSSPPSDL/PSP/SYSTEM/controls.ini ] && cp -Rf /usr/config/ppsspp/PSP/SYSTEM/ppsspp.ini /storage/roms/savestates/PPSSPPSDL/PSP/SYSTEM/controls.ini
+
+if [ ! -d "/storage/roms/savestates/PPSSPPSDL/PSP/Cheats" ] || [ -z "$(ls -A /storage/roms/savestates/PPSSPPSDL/PSP/Cheats 2>/dev/null)" ]; then
+    cp -Rf /usr/config/ppsspp/PSP/Cheats/. /storage/roms/savestates/PPSSPPSDL/PSP/Cheats
+fi
+ppssppcheevos.sh
+
 AUTOGP=$(get_ee_setting ppsspp_auto_gamepad)
 if [[ "${AUTOGP}" == "1" ]]; then
 	set_ppsspp_joy.sh
 fi
-
-ppssppcheevos.sh
-
-cp -Rf /usr/config/ppsspp/PSP/Cheats/. /storage/roms/savestates/PPSSPPSDL/PSP/Cheats
 
 ARG=${1//[\\]/}
 export SDL_AUDIODRIVER=alsa          
